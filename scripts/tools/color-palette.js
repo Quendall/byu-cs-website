3838f7fe1edd4e813882684329a19148f686c0d1
Kendall Clement <kendall_clement@outlook.com>
2022-02-24 11:56:37 -0700

    reorganized files, fixed http json error

--- scripts/tools/color-palette.js
+++ scripts/tools/color-palette.js
@@ -1,8 +1,5 @@
-fetchColors();
-
-document.getElementById("generate").addEventListener("click", function () {});
-
-function fetchColors() {
+document.getElementById("generate").addEventListener("click", function (event) {
+  event.preventDefault();
   let url = "http://colormind.io/api/";
   let data = {
     model: "default",
@@ -12,16 +9,12 @@ function fetchColors() {
   let http = new XMLHttpRequest();
 
   http.onreadystatechange = function () {
-    if ((http.readyState = 4 && http.status == 200)) {
+    if ((http.readyState == 4 && http.status == 200)) {
       let palette = JSON.parse(http.responseText).result;
-      insertColors(palette);
+      console.log(palette);
     }
   };
 
   http.open("POST", url, true);
   http.send(JSON.stringify(data));
-}
-
-function insertColors(palette) {
-  console.log(palette);
-}
+});
