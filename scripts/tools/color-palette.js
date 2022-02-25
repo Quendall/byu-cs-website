51b91ea933f2c6b7ff1343ad3d24584e9bfb643c
Kendall Clement <kendall_clement@outlook.com>
2022-02-24 17:04:19 -0700

    Fetch now updates color box elements

--- scripts/tools/color-palette.js
+++ scripts/tools/color-palette.js
@@ -3,16 +3,16 @@ document.getElementById("generate").addEventListener("click", function (event) {
   let url = "http://colormind.io/api/";
   let data = {
     model: "default",
-    input: [[44, 43, 44], [90, 83, 82], "N", "N", "N"],
+    input: [[44, 43, 44], "N", "N", "N", "N"],
   };
 
   let http = new XMLHttpRequest();
 
   http.onreadystatechange = function () {
-    if ((http.readyState == 4 && http.status == 200)) {
+    if (http.readyState == 4 && http.status == 200) {
       let palette = JSON.parse(http.responseText).result;
       console.log(palette);
-
+      changeColors(palette);
     }
   };
 
@@ -23,6 +23,10 @@ document.getElementById("generate").addEventListener("click", function (event) {
 function changeColors(palette) {
   let colors = Array.from(document.getElementsByClassName("color"));
   for (let i = 0; i < colors.length; i++) {
-    colors[i].style.backgroundColor = palette[i];
+    colors[i].style.backgroundColor = getRgbValue(palette[i]);
   }
+}
+
+function getRgbValue(color) {
+  return "rgb(" + color[0] + ", " + color[1] + ", " + color[2] + ")";
 }
